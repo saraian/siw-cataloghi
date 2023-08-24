7d106f3534b7b3dd8a97f233a26372095560698e
saraian <saraiannarelli01@gmail.com>
2023-08-24 22:53:20 +0200

    InitCommit

---
+++ siw-cataloghi-2/src/main/java/it/uniroma3/siw/SiwCataloghi2Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwCataloghi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwCataloghi2Application.class, args);
+	}
+
+}
